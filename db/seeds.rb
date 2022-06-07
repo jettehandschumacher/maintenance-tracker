2ba241a9693ed20d25185f96415c2a4a2e0bc52e
Jette <handschumacherjette@gmail.com>
2022-06-07 17:33:16 +0200

    wip - start seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require...
+
+User.destroy_all
+
+
+puts "Creating users..."
+jette = User.create!(email: "jette@test.com", password: "123456", first_name: "jette", last_name: 'handschuamcher')
+puts "Creating users..."
+ziggy = User.create!(email: "ziggy@test.com", password: "123456", first_name: "dziugas", last_name: "bartkevicius", technician: true)
