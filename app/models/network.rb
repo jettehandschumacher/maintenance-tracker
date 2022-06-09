8e0eb07ade418e6c13b0ea64491f5f040a69a807
Jette <handschumacherjette@gmail.com>
2022-06-09 19:12:18 +0200

    added ?

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,7 @@
 class Network < ApplicationRecord
   has_many :waterpoints
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :tasks
 
   def complete_tasks
