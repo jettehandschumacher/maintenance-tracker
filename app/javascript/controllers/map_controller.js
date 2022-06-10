ff5ad9d798c6ee346ef0dba8f019e36015e468d6
Jette <handschumacherjette@gmail.com>
2022-06-10 13:37:20 +0200

    show task for manager is ready

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/jettehandschumacher/cl487wvrw001014pf16pibtxq"
     })
 
     this.#addMarkersToMap()
