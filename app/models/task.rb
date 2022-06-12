310dca450a6916840063c3d05ed01a5519e7ae70
Dziugas Bartkevicius <bartkevicius.dziugas@gmail.com>
2022-06-12 18:15:13 +0100

    done for today not much else i cna add

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,10 +7,4 @@ class Task < ApplicationRecord
   belongs_to :service
   has_many :comments
   has_many :users, through: :comments
-  # has_many_attached :photos
-
-  def current_user_tasks(user)
-    Task.where("tasks.technician_id = ?", user.id).order('created_at asc').first
-  end
-
 end
