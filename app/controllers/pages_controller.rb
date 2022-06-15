960f0904a60f2d47844413dfaf7d17b8ae5d84c9
Louis Leslie <louis.leslie@gmail.com>
2022-06-15 10:29:07 +0100

    Add Chart data for tasks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,21 @@ class PagesController < ApplicationController
   end
 
   def statistics
+    @tasks_completed = Task.group(:complete_task).count
+    
+    @tasks_completed[:Complete] = @tasks_completed[true]
+    @tasks_completed[:Incomplete] = @tasks_completed[false]
+    @tasks_completed.except!(true)
+    @tasks_completed.except!(false)
+
+    @tasks_per_month = Task.where(complete_task: true).group_by_month(:end_date).count
+    p @tasks_per_month
+    # @tasks_Task.group_by_month(:end_date).count
+    @tasks_per_month = @tasks_per_month.transform_keys.with_index { |key| key.strftime("%b %Y") }
   end
 
   def presentation
   end
 end
+
+
