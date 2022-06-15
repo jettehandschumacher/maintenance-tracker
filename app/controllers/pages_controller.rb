515329d6ea2346564a085c96ac4d9867a7e35c39
Louis Leslie <louis.leslie@gmail.com>
2022-06-15 10:46:58 +0100

    Add Chart labels in pages_controller. remove four charts at top

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,11 @@ class PagesController < ApplicationController
     @tasks_per_month = Task.where(complete_task: true).group_by_month(:end_date).count
     p @tasks_per_month
     # @tasks_Task.group_by_month(:end_date).count
-    @tasks_per_month = @tasks_per_month.transform_keys.with_index { |key| key.strftime("%b %Y") }
+    @tasks_per_month = @tasks_per_month.transform_keys { |key| key.strftime("%b %Y") }
+
+    @split_tasks_per_month = Task.group(:complete_task).group_by_month(:end_date).count
+    p @split_tasks_per_month
+    @split_tasks_per_month = @split_tasks_per_month.transform_keys { |key| [key[0] == true ? "Complete" : "Incomplete", key[1].strftime("%b %Y")] }
   end
 
   def presentation
